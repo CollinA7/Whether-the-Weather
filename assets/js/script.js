e9369cc983b9a093e0ad91c3c45ebce634cdf6b4
Cayean <collinayala@gmail.com>
2022-08-23 00:57:39 -0500

    added functionality to submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
-// User inputs the city inthe search placeholder
+var cityFormEl = document.querySelector("#form-submit");
+var cityInputEl = document.querySelector("#city-input");
+var weatherContainer = document.querySelector("#display-city");
 
-// The API fetches the input city
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    console.log(cityInputEl.value)
 
-// the API returns the Temp, humidity, wind, and UV index for the searched city
+    // this variable collects the users input in the search bar
+    var city = cityInputEl.value.trim();
+    
+    // This conditional statemtent is 
+    if (city) {
+        getCityWeather(city);
+
+        // this clears the old content
+        // weatherContainer.textContent = "";
+        cityInputEl.value = "";
+    }else {
+        alert("please enter a valid city")
+    }
+};
+
+var getCityWeather = function(city) {
+
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&appid=4fce90cadf9eba29dd340628ab5664dc"
+        
+    fetch(apiUrl).then((response) => response.json()).then((data) => console.log(data));
+}
+
+$(cityFormEl).on("click", formSubmitHandler)
